147a2439bd6ae97a12cb5c5d0d24d2a6fa5421d2
Winston <winstonalfred48@gmail.com>
2025-01-02 10:16:19 +0700

    modified:   src/app/admin/page.tsx 	modified:   src/app/admin/page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,6 +3,8 @@ import NameTable from '@/components/NameTable'
 import { LotterySection } from '@/components/LotterySection'
 import { Suspense } from 'react'
 
+export const dynamic = 'force-dynamic';
+
 export default async function AdminPage() {
   return (
     <div className="container mx-auto py-10 space-y-8">
