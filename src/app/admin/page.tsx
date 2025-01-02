51ec098d70aff576c41d7bb7cbe2d1e4ae291372
Winston <winstonalfred48@gmail.com>
2025-01-02 10:07:56 +0700

    fix admin page not properly loading all the name

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,14 @@
 // src/app/admin/page.tsx
 import NameTable from '@/components/NameTable'
 import { LotterySection } from '@/components/LotterySection'
+import { Suspense } from 'react'
 
 export default async function AdminPage() {
   return (
     <div className="container mx-auto py-10 space-y-8">
+        < Suspense>
         <NameTable />
+        </Suspense>
         <LotterySection />
     </div>
   )
