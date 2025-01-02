a2d6bd2b1d86f50f0b1da7b9b59b8caf9bb883fd
Winston <winstonalfred48@gmail.com>
2025-01-02 10:01:10 +0700

    fix admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,12 @@
 // src/app/admin/page.tsx
-import AdminNameTable from '@/components/AdminNameTable'
+import NameTable from '@/components/NameTable'
 import { LotterySection } from '@/components/LotterySection'
 
 export default async function AdminPage() {
   return (
     <div className="container mx-auto py-10 space-y-8">
-      <AdminNameTable />
-      <LotterySection />
+        <NameTable />
+        <LotterySection />
     </div>
   )
 }
